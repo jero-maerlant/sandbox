0ba2b0290601009df13c93d11abef511046677ee
Jeroen Deserranno <jeroen.deserranno@pozyx.io>
2025-11-25 18:29:40 +0100

    docs: typo correction

--- copy_file.py
+++ copy_file.py
@@ -1,2 +1,2 @@
-# This file is a dummy file which is solely used for demo pruposes as this file needs to be copied from jero-maerlandt to the maerlant/sandbox repo
+# This file is a dummy file which is solely used for demo pruposes as this file needs to be copied from jero-maerlant to the GitHub Classroom maerlant/sandbox repo
 # The student should get familiar with copying a file from one repo to another one as they will need this for the exams.
